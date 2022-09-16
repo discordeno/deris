538ccc419eff0ee771182507011e0cdb0bac91c0
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-09-16 21:45:04 +0000

    why on earth did eris make this

--- src/Structures/NewsThreadChannel.ts
+++ src/Structures/NewsThreadChannel.ts
@@ -1,9 +1,10 @@
 import { DiscordChannel } from "discordeno/types";
 import { Client } from "../Client.js";
+import ThreadChannel from "./ThreadChannel.js";
 
-export class NewsThreadChannel {
-    constructor(data: DiscordChannel, client: Client) {
-        
+export class NewsThreadChannel extends ThreadChannel {
+    constructor(data: DiscordChannel, client: Client, messageLimit: number) {
+        super(data, client, messageLimit);
     }
 }
 
