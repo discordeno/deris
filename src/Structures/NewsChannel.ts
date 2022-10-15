4e3761efc1900a7d377f8aad65054ca3aff28433
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-10-15 14:54:43 +0000

    feat: NewsChannel

--- src/Structures/NewsChannel.ts
+++ src/Structures/NewsChannel.ts
@@ -1,11 +1,34 @@
-import { DiscordChannel } from "discordeno/types";
+import { BigString, DiscordChannel } from "discordeno/types";
 import { Client } from "../Client.js";
+import { ChannelFollow } from "../typings.js";
+import Message from "./Message.js";
 import TextChannel from "./TextChannel.js";
 
 export class NewsChannel extends TextChannel {
-    constructor(data: DiscordChannel, client: Client, messageLimit?: number) {
-        super(data, client, messageLimit);
-    }
+  constructor(data: DiscordChannel, client: Client, messageLimit?: number) {
+    super(data, client, messageLimit);
+
+    this.rateLimitPerUser = 0;
+    this.update(data);
+  }
+
+  /** Crosspost (publish) a message to subscribed channels */
+  async crosspostMessage(messageID: BigString): Promise<Message> {
+    return await this.client.crosspostMessage.call(
+      this.client,
+      this.id,
+      messageID
+    );
+  }
+
+  /** Follow this channel in another channel. This creates a webhook in the target channel */
+  async follow(webhookChannelID: BigString): Promise<ChannelFollow> {
+    return await this.client.followChannel.call(
+      this.client,
+      this.id,
+      webhookChannelID
+    );
+  }
 }
 
 export default NewsChannel;
--- src/Structures/TextChannel.ts
+++ src/Structures/TextChannel.ts
@@ -5,17 +5,21 @@ import Collection from "../Collection.js";
 import Message from "./Message.js";
 
 export class TextChannel extends GuildChannel {
-    messages: Collection<string, Message>;
+  /** Collection of Messages in this channel */
+  messages: Collection<string, Message>;
+  /** The ratelimit of the channel, in seconds. 0 means no ratelimit is enabled */
+  rateLimitPerUser: number | null;
 
-    constructor(data: DiscordChannel, client: Client, messageLimit?: number) {
-        super(data, client);
+  constructor(data: DiscordChannel, client: Client, messageLimit?: number) {
+    super(data, client);
 
-        this.messages = new Collection();
-        if (messageLimit == null) this.messages.limit = client.options.messageLimit;
-        else this.messages.limit = messageLimit;
-        
+    this.messages = new Collection();
+    if (messageLimit == null) this.messages.limit = client.options.messageLimit;
+    else this.messages.limit = messageLimit;
 
-    }
+    this.rateLimitPerUser =
+      data.rate_limit_per_user == null ? null : data.rate_limit_per_user;
+  }
 }
 
 export default TextChannel;
