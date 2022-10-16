7358ac7aa5c0edac6ed213b152ac1626be2a4b1b
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-10-16 17:52:21 +0000

    feat: stage channel

--- src/Structures/StageChannel.ts
+++ src/Structures/StageChannel.ts
@@ -1,11 +1,56 @@
 import { DiscordChannel } from "discordeno/types";
 import { Client } from "../Client.js";
+import { StageInstanceOptions } from "../typings.js";
+import StageInstance from "./StageInstance.js";
 import VoiceChannel from "./VoiceChannel.js";
 
 export class StageChannel extends VoiceChannel {
-    constructor(data: DiscordChannel, client: Client) {
-        super(data, client);
+  /** The topic of the channel */
+  topic?: string | null;
+
+  constructor(data: DiscordChannel, client: Client) {
+    super(data, client);
+  }
+
+  update(data: DiscordChannel) {
+    super.update(data);
+
+    if (data.topic !== undefined) {
+      this.topic = data.topic;
     }
+  }
+
+  /** Create a stage instance */
+  async createInstance(options: StageInstanceOptions): Promise<StageInstance> {
+    return await this.client.createStageInstance.call(
+      this.client,
+      this.id,
+      options
+    );
+  }
+
+  /** Delete the stage instance for this channel */
+  async deleteInstance(): Promise<void> {
+    return await this.client.deleteStageInstance.call(this.client, this.id);
+  }
+
+  /** Update the stage instance for this channel */
+  async editInstance(options: StageInstanceOptions): Promise<StageInstance> {
+    return await this.client.editStageInstance.call(
+      this.client,
+      this.id,
+      options
+    );
+  }
+
+  /** Get the stage instance for this channel */
+  async getInstance(): Promise<StageInstance> {
+    return await this.client.getStageInstance.call(this.client, this.id);
+  }
+
+  toJSON(props: string[] = []) {
+    return super.toJSON(["topic", ...props]);
+  }
 }
 
 export default StageChannel;
